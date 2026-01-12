d2ff71db0a4c24a66601db6a151a2cb8fc734467
drducqy95 <drducqy95@gmail.com>
2026-01-12 15:31:00 +0700

    Fix PWA: Increase Workbox cache limit to 50MB

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,zip}'],
         runtimeCaching: [
           {
